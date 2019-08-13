7681b7ad02746f9dd22f30c826ba3b86b9806eed
Lucianodasilvameyer <murcilhax@hotmail.com>
2019-08-12 22:31:34 -0300

    spawn da bola e duvidas

---
+++ Assets/Scripts/Bola.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bola : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!body || body == null)
+            body = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moverBola();
+    }
+    public void moverBola()
+    {
+        Vector2 input = new Vector2(0, 1);
+        Vector2 direction = input.normalized;
+        Vector2 velocity = speed * direction;
+        velocity.x = body.velocity.x;
+        body.velocity = velocity;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("colisor"))
+        {
+
+        }  
+    }
+
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField]
+    Plataforma plataforma_ref;
+
+    public GameObject bolaPrefab;
+
+    [SerializeField]
+    private float spawnarBolaInicial;
+
+    [SerializeField]
+    private float spawnarBolaMax;
+
+    [SerializeField]
+    private int distanciaDeQueda;
+
+    Vector2 lugarDeSpawnar;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+   void Update()
+   {
+      if(Time.time>=spawnarBolaInicial + spawnarBolaMax)
+      {
+            
+            spawnarBola(distanciaDeQueda, lugarDeSpawnar);
+            spawnarBolaInicial = Time.time;
+
+        }
+             
+   }
+   public void spawnarBola(int distanciaBolaPlataforma, Vector2 inicialPos)
+   {
+        inicialPos = plataforma_ref.transform.position;
+        Vector3 position = inicialPos;
+        float sizeX = plataforma_ref.GetComponent<SpriteRenderer>().bounds.size.x/2;
+        position.x = Random.Range(position.x - sizeX, position.x + sizeX);
+        position.y = distanciaBolaPlataforma;
+        position.z = -1;
+        GameObject ru = Instantiate(bolaPrefab, position, Quaternion.identity);
+
+   }
+   
+
+
+
+} 
